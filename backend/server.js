e6b7040e75581858cc2460f3b11bbcf740cd87f5
YakovTamam <Yaakovt100@gmail.com>
2022-08-19 11:44:16 +0300

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -21,10 +21,6 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 // using routes
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -38,6 +34,18 @@ app.get("/api/config/paypal", (req, res) => {
 // static folder
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
 
 // error handling middleware
 app.use(notFound);
