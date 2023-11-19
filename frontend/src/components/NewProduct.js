88bde6085b8eda569b95b5c010ca8f6714c2ca91
YakovTamam <Yaakovt100@gmail.com>
2023-11-19 18:43:53 +0200

    deploy

--- frontend/src/components/NewProduct.js
+++ frontend/src/components/NewProduct.js
@@ -11,9 +11,19 @@ export default function NewProduct(product) {
       <span className="product-p">₪3.86 / 100 מ"ל</span>
       <span>₪17.90</span>
       <div className="product-quantity">
-        <button className="product-button">+</button>
+        <button
+          className="product-button"
+          onClick={() => setQuantity(quantity + 1)}
+        >
+          +
+        </button>
         {quantity}
-        <button className="product-button">-</button>
+        <button
+          className="product-button"
+          onClick={() => setQuantity(quantity == 0 ? 0 : quantity - 1)}
+        >
+          -
+        </button>
       </div>
       <button className="product-addtocart">הוסף לעגלה</button>
     </button>
